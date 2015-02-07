edd45426b66079c70599489c473a8e7eec94a082
Dewayne VanHoozer <dvanhoozer@gmail.com>
2015-02-06 22:41:45 -0600

    looking at mqtt and libsvm for some telemetry work

---
+++ libsvm/libsvm_test.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'libsvm'
+
+# This library is namespaced.
+problem = Libsvm::Problem.new
+parameter = Libsvm::SvmParameter.new
+
+parameter.cache_size = 1 # in megabytes
+
+parameter.eps = 0.001
+parameter.c = 10
+
+samples = [ [1,0,1], [-1,0,-1] ]
+
+examples = samples.map {|ary| Libsvm::Node.features(ary) }
+
+labels = [1, -1]
+
+problem.set_examples(labels, examples)
+
+model = Libsvm::Model.train(problem, parameter)
+
+pred = model.predict(Libsvm::Node.features(1, 1, 1))
+puts "Example [1, 1, 1] - Predicted #{pred}"
