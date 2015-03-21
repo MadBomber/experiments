b574c80d6f493f48cacecb0445d55b9b2eb0cced
Dewayne VanHoozer <dvanhoozer@gmail.com>
2015-03-21 16:47:13 -0500

    kicking tires on wordnet-related gems

--- wordnet_related/words.rb
+++ wordnet_related/words.rb
@@ -1,63 +1,87 @@
-#!/usr/bin/env ruby -wKU
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'kick_the_tires'
+include KickTheTires
+
 require 'words'
 
-#data = Words::Wordnet.new # or: data = Words::Wordnet.new(:tokyo) for the tokyo backend
+#     data = Words::Wordnet.new
+# or: data = Words::Wordnet.new(:tokyo) for the tokyo backend
 
 # to specify a wordnet path Words::Words.new(:pure, '/path/to/wordnet')
-# to specify the tokyo dataset Words::Words.new(:pure, :search, '/path/to/data.tct')
+# to specify the tokyo dataset Words::Words.new(:tokyo, :search, '/path/to/data.tct')
 
 # Need to yum install wordnet wordnet-*
+# you can use :pure or :tokyo cabinet-based data stores
 data = Words::Wordnet.new(:pure, '/usr/share/wordnet-3.0/dict')
 
 
-
 # play with connections
-data.connected? # => true
+assert data.connected?
+
 data.close!
-data.connected? # => false
+
+refute data.connected?
+
 data.open!
-data.connected? # => true
-data.connection_type # => :pure or :tokyo depending...
+
+assert data.connected?
+
+assert_equal :pure, data.connection_type
 
 # locate a word
 lemma = data.find("bat")
 
-lemma.to_s # => bat, noun/verb
-lemma.available_pos.inspect # => [:noun, :verb]
+assert_equal 'bat, noun/verb', lemma.to_s
 
-lemma.synsets(:noun) # => array of synsets which represent nouns of the lemma bat
-# or
-lemma.nouns # => array of synsets which represent nouns of the lemma bat
-lemma.noun_ids # => array of synsets ids which represent nouns of the lemma bat
-lemma.verbs? #=> true
+assert_equal [:noun, :verb], lemma.available_pos.inspect
+
+show lemma.synsets(:noun)   # or lemma.nouns
+
+show lemma.noun_ids
+
+assert lemma.verbs?
 
 # specify a sense
-sense = lemma.nouns.last
+sense  = lemma.nouns.last
 sense2 = lemma.nouns[2]
 
-sense.gloss # => a club used for hitting a ball in various games
-sense2.words # => ["cricket bat", "bat"]
-sense2.lexical_description # => a description of the lexical meaning of the synset
-sense.relations.first # => "Semantic hypernym relation between n02806379 and n03053474"
+assert_equal 'a club used for hitting a ball in various games',
+                sense.gloss
 
-sense.relations(:hyponym) # => Array of hyponyms associated with the sense
-# or
-sense.hyponyms # => Array of hyponyms associated with the sense
-sense.hyponyms? # => true
+assert_equal ["cricket bat", "bat"], sense2.words
 
-sense.relations.first.is_semantic? # => true
-sense.relations.first.source_word # => nil
-sense.relations.first.destination # => the synset of n03053474
+show sense2.lexical_description
 
-sense.derivationally_related_forms.first.is_semantic? # => false
-sense.derivationally_related_forms.first.source_word # => "bat"
-sense.derivationally_related_forms.first.destination_word # => "bat"
-sense.derivationally_related_forms.first.destination # => the synset of v01413191
+assert_equal 'Semantic hypernym relation between n02806379 and n03053474',
+                sense.relations.first
+
+show sense.relations(:hyponym)  # or sense.hyponyms
+
+assert sense.hyponyms?
+
+assert sense.relations.first.is_semantic?
+
+assert_equal nil, sense.relations.first.source_word
+
+show sense.relations.first.destination
+
+refute sense.derivationally_related_forms.first.is_semantic?
+
+assert_equal 'bat', sense.derivationally_related_forms.first.source_word
+
+assert_equal 'bat', sense.derivationally_related_forms.first.destination_word
+
+show sense.derivationally_related_forms.first.destination
 
 if data.evocations? # check for evocation support
-   data.find("broadcast").senses.first.evocations # => sense relevant evocations
-   data.find("broadcast").senses.first.evocations[1] # => the evocation at index 1
-   data.find("broadcast").senses.first.evocations[1][:destination].words # => synset
+   # sense relevant evocations
+   show data.find("broadcast").senses.first.evocations
+   show data.find("broadcast").senses.first.evocations[1]
+   show data.find("broadcast").senses.first.evocations[1][:destination].words
 end
+
+hands_on
 
 data.close!
