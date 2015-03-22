35b48061bf05d3f020446cb788e202807e384569
Dewayne VanHoozer <dvanhoozer@gmail.com>
2015-03-21 22:53:28 -0500

    added the user_id search capability; changed search tea\rm from STDIN to command line (beause I wanted to) added creation of user_id index if it does not exist; playing with map reduce capabilty on user_id transaction counts

--- rethinkdb/search_db.rb
+++ rethinkdb/search_db.rb
@@ -26,14 +26,15 @@ thing = cli_helper("\nSearch for keywords in a localhost rethinkDB") do |o|
   o.string        '--db',    'database name',  default: 'simplifi_development'
   o.string  '-t', '--table', 'table  in which data is stored',  default: 'transactions'
   o.bool    '-i', '--insensitive', 'set case-insensitive', default: false
+  o.string  '-u', '--user',  'ignore the keywords and tell me about the user'
 
 end
 
-if $options[:arguments].empty?
+if $options[:arguments].empty?  &&  $options[:user].nil?
   error 'No search terms were specified.  See --help'
 else
   begin
-    db = ReDBH.new($options)
+    $db = RDB.new($options)
   rescue Exception => e
     error e
   end
@@ -41,9 +42,83 @@ end
 
 abort_if_errors
 
+
+unless $db.index_list.include?('user_id')
+  print 'Creating user_id index ... '
+  $db.create_simple_index('user_id')
+  print 'waiting ... '
+  $db.wait_on_index('user_id')
+  puts 'done'
+end
+
+
 ######################################################
 # Local methods
 
+def search_for_keywords
+  total_found = 0
+  # TODO: handle more than one keyword
+  keyword = $options[:arguments].first
+
+  if insensitive?
+    keyword = "(?i)#{keyword}"
+  end
+
+  cursor  = $db.search( url: keyword )
+
+  puts
+  puts "The following user_id values should get an ad"
+  puts "placement based upon the search term: #{keyword}"
+  puts
+
+  users = []
+
+  cursor.each do |c|
+    total_found += 1
+    if verbose?
+      puts
+      puts "="*45
+      pp c
+    end
+    users << c['user_id']
+  end
+
+  result = $db.get_all($db.r.args(users), index: 'user_id').group(:user_id).count.run
+
+  puts "\nRecap of user_id and their total transactions on file:"
+  ap result
+
+  puts
+
+  if verbose?
+    puts "\n\nTotal found: #{total_found}"
+    puts "To see more detail regarding why these users were chosen,"
+    puts "rerun the search using the -v, --verbose option."
+  end
+end # def search_for_keywords
+
+
+def search_for_user(user_id)
+  total_found = 0
+
+  cursor  = $db.search( user_id: user_id )
+
+  puts
+  puts "The following transactions were made by user ID #{$options[:user]}"
+  puts
+
+  cursor.each do |c|
+    total_found += 1
+    puts
+    puts "="*45
+    ap c
+  end
+
+  if verbose?
+    puts "\n\nTotal found: #{total_found}"
+  end
+
+end # def search_for_user
 
 ######################################################
 # Main
@@ -56,40 +131,18 @@ end
 
 ap $options  if verbose? || debug?
 
-# TODO: handle more than one keyword
-keyword = $options[:arguments].first
 
-if insensitive?
-  keyword = "(?i)#{keyword}"
+if $options[:user].nil?
+  search_for_keywords
+else
+  search_for_user $options[:user]
 end
 
-cursor  = db.search( url: keyword )
+puts "Users with more than 3 transactions:"
+users = $db.table.group(:user_id).count.gt(3).run
 
-puts <<EOS
-
-The following user_id values should get an ad
-placement based upon the search term: #{keyword}
-
-EOS
-
-cursor.each do |c|
-  if verbose?
-    puts
-    puts "="*45
-    pp c
-  else
-    puts c['user_id'] # FIXME: error in rethinkDB driver inconsistent use of symbol and string as keys
-  end
+users.each_pair do |k,v|
+  print "#{k}, " if v
 end
 
-puts
-
-unless verbose?
-  puts <<EOS
-To see more detail regarding why these users were chosen,
-rerun the search using the -v, --verbose option.
-
-EOS
-end
-
-db.close
+$db.close
