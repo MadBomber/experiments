e1bbbbf5cc56f03b6b32b0738010081defb4009f
Dewayne VanHoozer <dvanhoozer@gmail.com>
2015-08-29 16:43:21 -0500

    just playing

--- classifiers/train_svm_classifier.rb
+++ classifiers/train_svm_classifier.rb
@@ -3,12 +3,20 @@
 ###
 ##  File: train_svm_classifier.rb
 ##  Desc: Train a classifier for the current directory.
+##
+##  Makes use of the following common *nix pgms:
+##    html2text
+##    pdf2txt
+##    catdoc
+##    strings
 #
+
+require 'bag_of_words'
+require 'awesome_print'
 
 require 'pathname'  # STDLIB
 require 'hoatzin'   # GEM that implements an SVM-based classifier
 #require 'summary'   # GEM that summarizes a document
-require 'systemu'   # STDLIB
 require 'pp'        # STDLIB
 
 stored_classifier_name  = 'classifier'
@@ -16,53 +24,53 @@ scn_metadata            = stored_classifier_name + '.metadata'
 scn_model               = stored_classifier_name + '.model'
 
 $stop_words = %w(
-  able about above abroad according accordingly across actually adj after afterwards again 
-  against ago ahead ain't all allow allows almost alone along alongside already also although 
-  always am amid amidst among amongst an and another any anybody anyhow anyone anything anyway 
-  anyways anywhere apart appear appreciate appropriate are aren't around as a's aside ask asking 
-  associated at available away awfully back backward backwards be became because become becomes 
-  becoming been before beforehand begin behind being believe below beside besides best better 
-  between beyond both brief but by came can cannot cant can't caption cause causes certain 
-  certainly changes cid clearly c'mon co co. com come comes concerning consequently consider 
-  considering contain containing contains corresponding could couldn't course c's currently 
-  dare daren't definitely described despite did didn't different directly do does doesn't 
-  doing done don't down downwards during each edu eg eight eighty either else elsewhere 
-  end ending enough entirely especially et etc even ever evermore every everybody everyone 
-  everything everywhere ex exactly example except fairly far farther few fewer fifth first 
-  five followed following follows for forever former formerly forth forward found four from 
-  further furthermore get gets getting given gives go goes going gone got gotten greetings 
-  had hadn't half happens hardly has hasn't have haven't having he he'd he'll hello help 
-  hence her here hereafter hereby herein here's hereupon hers herself he's hi him himself 
-  his hither hopefully how howbeit however hundred i'd ie if ignored i'll i'm immediate in 
-  inasmuch inc inc. indeed indicate indicated indicates inner inside insofar instead into 
-  inward is isn't it it'd it'll its it's itself i've just k keep keeps kept know known 
-  knows last lately later latter latterly least less lest let let's like liked likely likewise 
-  little look looking looks low lower ltd made mainly make makes many may maybe mayn't me 
-  mean meantime meanwhile merely might mightn't mine minus miss more moreover most mostly mr 
-  mrs much must mustn't my myself name namely nd near nearly necessary need needn't needs 
-  neither never neverf neverless nevertheless new next nine ninety no nobody non none nonetheless 
-  noone no-one nor normally not nothing notwithstanding novel now nowhere obviously of off 
-  often oh ok okay old on once one ones one's only onto opposite or other others otherwise 
-  ought oughtn't our ours ourselves out outside over overall own particular particularly past 
-  per perhaps placed please plus possible presumably probably provided provides que quite qv 
-  rather rd re really reasonably recent recently regarding regardless regards relatively 
-  respectively right round said same saw say saying says second secondly see seeing seem 
-  seemed seeming seems seen self selves sensible sent serious seriously seven several shall 
-  shan't she she'd she'll she's should shouldn't since six so some somebody someday somehow 
-  someone something sometime sometimes somewhat somewhere soon sorry specified specify 
-  specifying still sub such sup sure take taken taking tell tends th than thank thanks 
-  thanx that that'll thats that's that've the their theirs them themselves then thence 
-  there thereafter thereby there'd therefore therein there'll there're theres there's 
-  thereupon there've these they they'd they'll they're they've thing things think third 
-  thirty this thorough thoroughly those though three through throughout thru thus till 
-  to together too took toward towards tried tries truly try trying t's twice two un 
-  under underneath undoing unfortunately unless unlike unlikely until unto up upon 
-  upwards us use used useful uses using usually v value various versus very via viz 
-  vs want wants was wasn't way we we'd welcome well we'll went were we're weren't we've 
-  what whatever what'll what's what've when whence whenever where whereafter whereas 
-  whereby wherein where's whereupon wherever whether which whichever while whilst whither 
-  who who'd whoever whole who'll whom whomever who's whose why will willing wish with 
-  within without wonder won't would wouldn't yes yet you you'd you'll your you're 
+  able about above abroad according accordingly across actually adj after afterwards again
+  against ago ahead ain't all allow allows almost alone along alongside already also although
+  always am amid amidst among amongst an and another any anybody anyhow anyone anything anyway
+  anyways anywhere apart appear appreciate appropriate are aren't around as a's aside ask asking
+  associated at available away awfully back backward backwards be became because become becomes
+  becoming been before beforehand begin behind being believe below beside besides best better
+  between beyond both brief but by came can cannot cant can't caption cause causes certain
+  certainly changes cid clearly c'mon co co. com come comes concerning consequently consider
+  considering contain containing contains corresponding could couldn't course c's currently
+  dare daren't definitely described despite did didn't different directly do does doesn't
+  doing done don't down downwards during each edu eg eight eighty either else elsewhere
+  end ending enough entirely especially et etc even ever evermore every everybody everyone
+  everything everywhere ex exactly example except fairly far farther few fewer fifth first
+  five followed following follows for forever former formerly forth forward found four from
+  further furthermore get gets getting given gives go goes going gone got gotten greetings
+  had hadn't half happens hardly has hasn't have haven't having he he'd he'll hello help
+  hence her here hereafter hereby herein here's hereupon hers herself he's hi him himself
+  his hither hopefully how howbeit however hundred i'd ie if ignored i'll i'm immediate in
+  inasmuch inc inc. indeed indicate indicated indicates inner inside insofar instead into
+  inward is isn't it it'd it'll its it's itself i've just k keep keeps kept know known
+  knows last lately later latter latterly least less lest let let's like liked likely likewise
+  little look looking looks low lower ltd made mainly make makes many may maybe mayn't me
+  mean meantime meanwhile merely might mightn't mine minus miss more moreover most mostly mr
+  mrs much must mustn't my myself name namely nd near nearly necessary need needn't needs
+  neither never neverf neverless nevertheless new next nine ninety no nobody non none nonetheless
+  noone no-one nor normally not nothing notwithstanding novel now nowhere obviously of off
+  often oh ok okay old on once one ones one's only onto opposite or other others otherwise
+  ought oughtn't our ours ourselves out outside over overall own particular particularly past
+  per perhaps placed please plus possible presumably probably provided provides que quite qv
+  rather rd re really reasonably recent recently regarding regardless regards relatively
+  respectively right round said same saw say saying says second secondly see seeing seem
+  seemed seeming seems seen self selves sensible sent serious seriously seven several shall
+  shan't she she'd she'll she's should shouldn't since six so some somebody someday somehow
+  someone something sometime sometimes somewhat somewhere soon sorry specified specify
+  specifying still sub such sup sure take taken taking tell tends th than thank thanks
+  thanx that that'll thats that's that've the their theirs them themselves then thence
+  there thereafter thereby there'd therefore therein there'll there're theres there's
+  thereupon there've these they they'd they'll they're they've thing things think third
+  thirty this thorough thoroughly those though three through throughout thru thus till
+  to together too took toward towards tried tries truly try trying t's twice two un
+  under underneath undoing unfortunately unless unlike unlikely until unto up upon
+  upwards us use used useful uses using usually v value various versus very via viz
+  vs want wants was wasn't way we we'd welcome well we'll went were we're weren't we've
+  what whatever what'll what's what've when whence whenever where whereafter whereas
+  whereby wherein where's whereupon wherever whether which whichever while whilst whither
+  who who'd whoever whole who'll whom whomever who's whose why will willing wish with
+  within without wonder won't would wouldn't yes yet you you'd you'll your you're
   yours yourself yourselves you've zero
   software system project manager managers
   microsoft word document
@@ -82,16 +90,15 @@ def keywords(string_of_words)
       words[w] += 1
     end
   end
-  
-  cutoff = words.values.max.to_f / 3.0
-  
-  return words.select {|k,v| v > cutoff}.keys
+
+  cutoff = words.values.max.to_f / 4.0
+
+  return words.select {|k,v| v >= cutoff}.keys
 
 end
 
 
 class Pathname
-
   def is_archive?
     case extname.downcase
       when '.bz2', '.tgz', '.gz', '.tar', '.zip' then
@@ -100,7 +107,6 @@ class Pathname
         false
     end
   end
-
 end
 
 
@@ -109,12 +115,53 @@ class String
     self.split(" ").select{|w| 2<w.length and !$stop_words.include?(w)}.join(" ")
   end
   def remove_stuff(seq="\n")
-#    self.gsub(seq, ' ').gsub(/[_\-\.\,0-9\W]/, ' ').strip.squeeze(" ").remove_stop_words
     self.gsub(seq, ' ').gsub(/[^a-z]/, ' ').strip.squeeze(" ").remove_stop_words
   end
 end # class String
 
 
+
+def summarize_file(a_path)
+  #GC::start
+  puts
+  puts "   File: #{a_path.basename.to_s}"
+  ext       = a_path.extname.downcase
+  title     = a_path.basename.to_s.downcase.remove_stuff(ext)
+  puts "  Title: #{title}"
+
+  summary   = title + " "
+
+  case ext
+    when '.html', '.htm' then
+      text = `html2text '#{a_path.to_s}'`
+      summary += text.downcase.remove_stuff      # .summary(1500, " ")
+      summary = keywords(summary).join(', ')
+
+    when '.pdf' then
+      text = `pdf2txt '#{a_path.to_s}'`
+      summary += text.downcase.remove_stuff      # .summary(1500, " ")
+      summary = keywords(summary).join(', ')
+
+    when '.doc', '.docx' then
+      text = `catdoc '#{a_path.to_s}'`
+      summary += text.gsub(/[^a-zA-Z]/,' ').downcase.remove_stuff     # .summary(1500, " ")
+      summary = keywords(summary).join(', ')
+
+    when '.txt' then
+      text = a_path.read
+      summary += text.downcase.remove_stuff     # .summary(1500, " ")
+      summary = keywords(summary).join(', ')
+
+    else
+      text = `strings '#{a_path.to_s}'`
+      summary += text.downcase.remove_stuff     # .summary(1500, " ")
+      summary = keywords(summary).join(', ')
+  end # end case ext
+
+  puts "Summary: #{summary}"
+
+  return summary
+end # end of def summarize_file(a_path)
 
 # The top-level directory holding both sub-directories of
 # already classified files and at the top level, files which
@@ -137,7 +184,11 @@ end
 puts "Train a classifier (#{category_hash.keys}) for this directory: #{cwd}"
 
 # These are files which have not been categorized
-files_without_category = cwd.children.select {|c| c if c.file? }
+files_without_category = cwd.children.select do |c|
+  c if c.file?  &&
+       '.model'    != c.extname.to_s.downcase &&
+       '.metadata' != c.extname.to_s.downcase
+end
 
 if scn_metadata_path.exist?
   classifier = Hoatzin::Classifier.new( :metadata => scn_metadata_path.to_s,
@@ -150,58 +201,31 @@ end
 category_hash.each_pair do | category, dir |
 
   puts "-" * 45
-  
+
   classifier.train category, category.remove_stuff
-        
+
   puts "Training #{category} ..."
+
+
   dir.children.each do |f|
 
+    bag = BagOfWords.new
+
     unless f.directory?
-      
-      GC::start
-      
-      ext       = f.extname.downcase
-      title     = f.basename.to_s.downcase.remove_stuff(ext)
-      puts
-      puts "   File: #{f.basename.to_s}"
-      puts "  Title: #{title}"
-      classifier.train category, title
-      summary = title + " "
-      case ext
-        when '.html', '.htm' then
-          a,b,c = systemu "html2text '#{f.to_s}'"
-          summary += b.downcase.remove_stuff     # .summary(1500, " ")
-          summary = keywords(summary).join(', ')
-          
-        when '.pdf' then
-          a,b,c = systemu "pdf2txt '#{f.to_s}'"
-          summary += b.downcase.remove_stuff     # .summary(1500, " ")
-          summary = keywords(summary).join(', ')
-
-#        when '.doc' then
-#          a,b,c = systemu "catdoc '#{f.to_s}'"
-#          summary += b.downcase.remove_stuff     # .summary(1500, " ")
-#          summary = keywords(summary).join(', ')
-
-        when '.txt' then
-          fc = f.read
-          summary += fc.downcase.remove_stuff    # .summary(1500, " ")
-          summary = keywords(summary).join(', ')
-
-        else
-          a,b,c = systemu "strings '#{f.to_s}'"
-          summary += b.downcase.remove_stuff     # .summary(1500, " ")
-          summary = keywords(summary).join(', ')
-
-      end
+      summary = summarize_file(f)
       unless summary.empty?
-        puts "Summary: #{summary}"
+        bag.add_doc summary
         classifier.train category, summary
       end
     end
+
+    ap bag
+
   end
-  
+
 end
+
+
 
 
 # Force the calculation of the feature vectors and
@@ -220,57 +244,15 @@ puts "=" * 45
 
 files_without_category.each do |f|
 
-  # TODO: skip the classifier's metadata and model files
-  
+  summary = summarize_file(f)
 
-  GC::start
-
-  puts
-  puts "   File: #{f.basename.to_s}"
-  ext       = f.extname.downcase
-  title     = f.basename.to_s.downcase.remove_stuff(ext)
-  puts "  Title: #{title}"
-
-  summary   = title + " "
-  
-  case ext
-    when '.html', '.htm' then
-      a,b,c = systemu "html2text '#{f.to_s}'"
-      summary += b.downcase.remove_stuff      # .summary(1500, " ")
-      summary = keywords(summary).join(', ')
-      
-    when '.pdf' then
-      a,b,c = systemu "pdf2txt '#{f.to_s}'"
-      summary += b.downcase.remove_stuff      # .summary(1500, " ")
-      summary = keywords(summary).join(', ')
-
-#    when '.doc' then
-#      a,b,c = systemu "catdoc '#{f.to_s}'"
-#      summary += b.gsub(/[^a-zA-Z]/,' ').downcase.remove_stuff     # .summary(1500, " ")
-#      summary = keywords(summary).join(', ')
-      
-    when '.txt' then
-      fc = f.read
-      summary += fc.downcase.remove_stuff     # .summary(1500, " ")
-      summary = keywords(summary).join(', ')
-      
-    else
-      a,b,c = systemu "strings '#{f.to_s}'"
-      summary += b.downcase.remove_stuff     # .summary(1500, " ")
-      summary = keywords(summary).join(', ')
-
-  end
-
-  
-  
   category  = classifier.classify summary
-  puts "Summary: #{summary}"
   puts
   puts "Category: #{category}"
   puts
   print "Do you want to move the file? (no) "
   answer = gets.strip.downcase
-  if !answer.empty? and 'y' == answer[0]
+  if !answer.empty? && 'y' == answer[0]
     system "mv -i '#{f}' '#{category_hash[category]}'"
   end
 end
@@ -290,4 +272,8 @@ for each sub-directory
 
 store the classifier in the directory
 
-
+for each unknown file in the directory
+  get a summary
+  classify the summary
+  move the file into the proper sub-directory as
+    directed by the user
