1bdd6e378a72e85df423b179c2651fbacbdacc71
Dewayne VanHoozer <dvanhoozer@42six.com>
2015-09-05 23:25:19 -0500

    changed to cluster on entire document not just its individual lines/paragraphs

--- classifiers/kmeans_clusterer_test.rb
+++ classifiers/kmeans_clusterer_test.rb
@@ -5,6 +5,8 @@
 # TODO: Add CliHelper; get data files from command line; allow directory recursion
 # TODO: allow for skip extensions - extensions of files to be skipped
 
+require 'awesome_print'
+
 require 'debug_me'
 include DebugMe
 
@@ -14,49 +16,91 @@ include CliHelper
 require 'pathname'
 
 require 'kmeans-clusterer'
+
+# from my lib/ruby
 require 'bag_of_words'
-require 'optparse'
 
-
-cwd = Pathname.pwd
-training_data = cwd + 'training_data'
-
+configatron.version = '0.0.1'
 
 # just using text files to make things easier
-datafiles = Dir['training_data/**/*.txt']
-basenames = datafiles.map {|f| File.basename(f, '.txt')}
+default_datafiles = Dir['training_data/**/*.txt']
 
-target_cluster_size = datafiles.length
-max_runs = 10
 
-OptionParser.new do |opts|
-  opts.on("-kK") {|v| target_cluster_size = v.to_i }
-  opts.on("-rD") {|v| max_runs = v.to_i }
-end.parse!
+HELP = <<EOHELP
+Important:
+
+  I'm not sure that I like k-means for document clustering.
+
+  The kmeans-clusterer library relies upon everthing being in memory.
+
+EOHELP
+
+cli_helper("Cluster Some Documents") do |o|
+
+  o.int     '-r', '--max_runs',    			'Maximum Number Of Runs To Perform', 	default: 10
+  o.int 	'-c', '--target_cluster_size',  'Target Cluster Size',   				default: 5
+  o.paths   '-f', '--files',  'Files to Cluster', default: default_datafiles.map{|f| Pathname.new f}
+
+end
+
+# Display the usage info
+if  ARGV.empty?
+  show_usage
+  exit
+end
+
+
+# Error check you stuff; use error('some message') and warning('some message')
+
+abort_if_errors
+
+
+max_runs 			= configatron.max_runs
+target_cluster_size = configatron.target_cluster_size
+datafiles 			= configatron.files
+basenames           = datafiles.map {|f| f.basename.to_s.gsub(f.extname,'')}
 
 
 docs = [] 			# docs is really text entries (ie lines or paragraphs) from a document
 doc_fileids = []    # index to the actual file from which the line|paragraph came
+
+bag = BagOfWords.new idf: true
+
+
+######################################################
+# Local methods
+
 
 get_basename = -> (docid) {
   fileid = doc_fileids[docid]
   basenames[fileid]
 }
 
-bag = BagOfWords.new idf: true
 
+######################################################
+# Main
 
-# bring all the entries from all the files into memory
-
-datafiles.each_with_index do |filename, i|
-  File.open(filename).each do |line|
-    doc = line.chomp
-    next if doc.empty?
-    bag.add_doc doc
-    docs << doc
-    doc_fileids << i
-  end
+at_exit do
+  puts
+  puts "Done."
+  puts
 end
+
+ap configatron.to_h  if verbose? || debug?
+
+
+
+# bring all the files into memory
+
+datafiles.each_with_index do |path_name, i|
+  doc = path_name.read
+  next if doc.empty?
+  bag.add_doc doc
+  docs << doc
+  doc_fileids << i
+end
+
+
 
 puts "\nClassifying #{docs.length} entries with #{bag.terms_count} unique terms into #{target_cluster_size} clusters:\n"
 data = bag.to_matrix
@@ -67,6 +111,33 @@ data = bag.to_matrix
 start 	= Time.now
 kmeans 	= KMeansClusterer.run(target_cluster_size, data, runs: max_runs, log: true)
 elapsed = Time.now - start
+
+
+ap kmeans
+
+#ap kmeans.methods
+
+interesting_methods = [
+	#:centroids,
+	:clusters,
+	#:data,
+	:distances,
+	:iterations,
+	:k,
+	:points,
+	#:predict(data),
+	:run,
+	:runtime,
+	:silhouette
+]
+
+interesting_methods.each do |i_method|
+	puts
+	puts "="*45
+	puts "== Method: #{i_method}"
+	result = kmeans.send(i_method)
+	puts "Result: #{result}"
+end
 
 
 # Report the results
