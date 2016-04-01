dec10f4dd77edcd1ba4381b9272f00200ab0a9f0
Dewayne VanHoozer <dvanhoozer@42six.com>
2016-04-01 15:12:03 -0500

    rewrote to make use of Sycamore::Tree

--- threat_analysis/threat_analysis.rb
+++ threat_analysis/threat_analysis.rb
@@ -1,78 +1,115 @@
 #!/usr/bin/env ruby
 #############################################
 ###
-## 	File: threat_analysis.rb
+##  File: threat_analysis.rb
 ##  Desc: notional thought on object relationships
-#
-=begin
-  
-What I seem to be moving toward is a graph in which the
-indention represents inclusion and the first word
-represents an edge 'name'
+##
+# TODO: given a new top-level node, find all existing sub-nodes
+#       that match given name, value and attach new information to
+#       those sub-nodes.  In other words, don't create a top-level
+#       node if the information is already in the tree at a lower
+#       level.  Does this mean that there might be duplicate sub-nodes? yes.
+#       Is that bad? yea, but will deal with that later.
 
-For example
+DEBUG = ARGV.include?('--debug')
+def debug?; DEBUG; end
 
-node name
-  is_a top_level_node
-
-would generate a graph component like:
-
-  "node name"  --- "is_a" --=> "top_level_node"
-
-I should be able to represent this in dot notation for graphvis
-
-This almost sounds like a mindmap.
-
-=end
-
-class Node
-  def initialize(name, &block)
-    @name = name
-    @children = []
-
-    instance_eval(&block) if block
-  end
-
-  attr_reader :children, :name
-
-  def node(name, &block)
-    children << Node.new(name, &block)
-  end
-end
-
-
-Node.new('root') do
-  node('branch') do
-    node('leaf')
-    node('leaf2')
-    node('leaf3')
-  end
-end
-
-
-require 'hashie'
 require 'awesome_print'
+require 'debug_me'
+include DebugMe
 
-db = Array.new
+require 'sycamore'
 
-class DbEntry < Hashie::Mash
+module Sycamore
+  class Tree
+    def search(a_string)
+      self.each_path.select{|a_path| a_path.join('/').downcase.include?(a_string.downcase)}
+    end
+  end
 end
 
-dsl_commands = Array.new
+notes = Sycamore::Tree.new
+
+spaces_per_tab = "  "
+
+def extract_name_value(a_string)
+  a_line  = a_string.strip
+  parts   = a_line.split
+  name    = parts.shift
+  return name.to_sym, parts.join(' ')
+end
+
+prev_indent = 0
+
+node_stack  = []
 
 DATA.each_line do |a_line|
-  puts a_line
-  left_shifted = a_line.strip
-  next if left_shifted.empty?
-  next if left_shifted.start_with? '#'
-  left_shifted_parts = left_shifted.split
-  command = left_shifted_parts.shift.downcase
-  rest = left_shifted_parts
-  dsl_commands << command unless  dsl_commands.include? command
+  a_line.chomp!
+  next if a_line.strip.empty?
+  next if a_line.lstrip.start_with?('#')
+
+  a_line.gsub!("\t", spaces_per_tab)
+
+  name, value = extract_name_value(a_line)
+
+  indent_level = a_line.length - a_line.lstrip.length
+
+  if 0 == indent_level
+    node_stack  = []
+    notes[name] << value
+    node_stack.push [indent_level, name, value]
+  else
+    last_node = node_stack.last
+
+    if indent_level == last_node[0]
+      node_stack.pop 
+    elsif indent_level < last_node[0]
+      until (last_node[0] < indent_level) do        
+        node_stack.pop
+        last_node = node_stack.last
+      end
+    end
+    
+    path_to_here = []
+
+    node_stack.each do |node|
+      path_to_here << node[1].to_sym
+      path_to_here << node[2]
+    end
+
+    path_to_here << name
+
+    node_stack.push [indent_level, name, value]
+
+    notes[ path_to_here ] << value
+
+  end
+
+ puts node_stack.inspect if debug?
+
 end
 
-ap dsl_commands
+if debug?
+  ap notes.to_h
 
+  notes.each_path do |a_path|
+    puts a_path.join('/')
+  end
+
+  ap notes.search('leader')
+end # if testing?
+
+unless ARGV.empty?
+  ARGV.each do |search_term|
+    next if search_term.start_with?('-')
+    puts "\n\n"
+    puts "#"*45
+    puts "## Search term: #{search_term}"
+    ap notes.search(search_term)
+  end
+else
+  puts "\n\nAdd some search terms to the command line.  See what pops out.\n\n"
+end
 
 __END__
 
@@ -98,11 +135,12 @@ explosive PETN
   is_a powdery
   is_a difficult-to-detect explosive
 
+explosive TAPT 
+
 threat Tehreek-e-Taliban Pakistan
   aka TTP
   person Hafez Saeed Khan
     is_a commander
-
 
 person Abu Muhammad al-Adnani
   aka al-Adnani
@@ -117,34 +155,34 @@ threat Khorasan
   is_a group of senior al-Qaeda members
   ideology Salafi jihadism
     defined_as a transnational religious-political ideology based on a belief in violent jihad and the Salafist religious movement of returning to (what adherents believe is) "true" Sunni Islam
-  plans_to strik targets in the United States and Europe
+  plans_to strike targets in the United States and Europe
   composed_of former Al-Qaeda operatives
-    from Middle East
-    from North Africa
-    from South Asia
+    area Middle East
+    area North Africa
+    area South Asia
   terror_method concealed explosives
   person Abdelrahman al Johani
     aka Al Johani
     is_a bomb_maker
     is_a counterintelligence chief
-    born 1970
-      in Saudi Arabia
-    trained explosives
-    trained toxins
+    date_born 1970
+      area Saudi Arabia
+    trained_in explosives
+    trained_in toxins
   person Mohammed Islambouli
   person Abdul al Charekh
     is_a internet propagandist
     is_a money_man
-    born 1985
+    date_born 1985
   person Adel Radi Saqr al-Wahabi al-Harbi
     is_a leader
-    died march 2015
-      in Idlib, Syria
+    date_died march 2015
+      area Idlib, Syria
   person David Drugeon
     nationality french
     is_a bomber_maker
-    died march 2015
-      in Idlib, Syria
+    date_died march 2015
+      area Idlib, Syria
   person Muhsin al-Fadhli
     aka Abu Majid Samiyah
     aka Abu Samia
@@ -153,23 +191,23 @@ threat Khorasan
     aka Muhsin Fadil Ayid Ashur al-Fadhli
     is_a senior_leader
     close_to Osama bin Laden
-    born 24 April 1981
-      in  Kuwait
-    died 8 July 2015
-      in Sarmada, Syria
+    date_born 24 April 1981
+      area  Kuwait
+    date_died 8 July 2015
+      area Sarmada, Syria
     credited_by US State Department
-      in 2012
+      date 2012
       as leader of the Iranian branch of Al-Qaeda
     works_with wealthy “jihadist donors”
-      in Kuwait
-	credited_by Former President George W. Bush
-	  in 2005
-	  as leader
-	  event French oil tanker bombing
-	    in 2002
+      area Kuwait
+  credited_by Former President George W. Bush
+    date 2005
+    as leader
+    event French oil tanker bombing
+      date 2002
   person al-Zawahiri
     is_a senior_leader
-    influence weak
+    influence_is weak
   person Ibrahim al-Asiri 
     is_a bomb_maker
     linked_to  Al-Qaeda
@@ -180,7 +218,7 @@ threat Khorasan
     as new arm in attacking America
   linked_to Al Nusra
     as allies
-    in Syria
+    area Syria
   scope global
 
 ############################################
@@ -197,7 +235,7 @@ threat Al Nusra
   aka Jabhat al Nusra
   linked_to Al-Qaeda
     as official branch
-    in Syria
+    area Syria
 
 ############################################
 threat Ahrar al Sham
