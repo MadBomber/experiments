94c6eadcd0cc1a87c58da98a462f33d7bd6a31e5
Dewayne VanHoozer <dvanhoozer@42six.com>
2016-04-03 11:59:39 -0500

    checkpoint

--- mail/read_some_mail.rb
+++ mail/read_some_mail.rb
@@ -7,7 +7,7 @@ include DebugMe
 require 'mail'
 
 Mail.defaults do
-  retriever_method :imap, :address    => "108.177.8.109",  # "imap.gmail.com",
+  retriever_method :imap, :address    => "imap.gmail.com",
                           :port       => 993,
                           :user_name  => ENV['GMAIL_USER'],
                           :password   => ENV['GMAIL_PASS'],
@@ -15,7 +15,53 @@ Mail.defaults do
 end
 
 
-emails = Mail.find(:what => :first, :count => 1, :order => :asc, :keys => 'ALL')
+emails = Mail.find(:what => :last, :count => 2, :order => :asc, :keys => 'ALL')
 
 
-debug_me{[ :emails ]}
+ap emails.first.methods
+
+
+emails.each do |mail|
+
+  print "\n\n"
+
+  debug_me{[ 
+    #'mail.header_fields',
+    #'mail.headers',
+    #'mail.envelope_from',   #=> 'mikel@test.lindsaar.net'
+    'mail.from',
+    'mail.from_addrs',
+    #'mail.from.addresses',  #=> ['mikel@test.lindsaar.net', 'ada@test.lindsaar.net']
+    #'mail.sender.address',  #=> 'mikel@test.lindsaar.net'
+    'mail.to',              #=> 'bob@test.lindsaar.net'
+    'mail.cc',              #=> 'sam@test.lindsaar.net'
+    'mail.subject',         #=> "This is the subject"
+    'mail.date.to_s',       #=> '21 Nov 1997 09:55:06 -0600'
+    'mail.message_id',      #=> '<4D6AA7EB.6490534@xxx.xxx>'
+    #'mail.body.decoded',    #=> 'This is the body of the email...
+    'mail.body',
+    'mail.html_part.methods',
+    'mail.text_part.methods'
+  ]}
+
+end
+
+
+
+__END__
+
+mail.attachments.each do | attachment |
+  # Attachments is an AttachmentsList object containing a
+  # number of Part objects
+  if (attachment.content_type.start_with?('image/'))
+    # extracting images for example...
+    filename = attachment.filename
+    begin
+      File.open(images_dir + filename, "w+b", 0644) {|f| f.write attachment.body.decoded}
+    rescue => e
+      puts "Unable to save data for #{filename} because #{e.message}"
+    end
+  end
+end
+
+
