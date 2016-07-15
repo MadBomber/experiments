f89749a99fcac73818e56208f270a0ea33335951
Dewayne VanHoozer <dvanhoozer@42six.com>
2016-07-15 11:48:15 -0500

    added comments to remind how to run this web app

--- java_junk/play_with_gradel_and_ratpack/hello_ratpack.groovy
+++ java_junk/play_with_gradel_and_ratpack/hello_ratpack.groovy
@@ -1,3 +1,7 @@
+// hello_ratpack.groovey
+// To execute this web app do:
+//      groovey hello_ratpack.groovey
+
 @Grapes([
   @Grab('io.ratpack:ratpack-groovy:1.3.3'),
   @Grab('org.slf4j:slf4j-simple:1.7.12')
