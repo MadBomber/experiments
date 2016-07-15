a3fca12d71b515bd221c9366bdb732d1e9d1e3c9
Dewayne VanHoozer <dvanhoozer@42six.com>
2016-07-15 16:09:46 -0500

    added another get handler per the tutorial

--- java_junk/jruby_with_ratpack/server.rb
+++ java_junk/jruby_with_ratpack/server.rb
@@ -1,4 +1,7 @@
 # server.rb
+# Reference: https://blog.heroku.com/reactive_ruby_building_real_time_apps_with_jruby_and_ratpack
+
+
 
 require 'java' 
 require 'jruby/core_ext'
@@ -7,10 +10,23 @@ Bundler.require
 
 java_import 'ratpack.server.RatpackServer'
 
+java_import 'ratpack.stream.Streams'
+java_import 'ratpack.http.ResponseChunks'
+java_import 'java.time.Duration'
+
+
 RatpackServer.start do |b| 
   b.handlers do |chain| 
     chain.get do |ctx| 
       ctx.render("Hello from Ratpack+JRuby") 
     end
+
+    chain.get("stream") do |ctx|
+      publisher = Streams.periodically(ctx, Duration.ofMillis(1000)) do |i|
+        i < 10 ? i.to_s : nil
+      end
+      ctx.render(ResponseChunks.stringChunks(publisher))
+    end
+
   end
 end
