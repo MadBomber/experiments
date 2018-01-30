9e34f08a25e9ba56f7098d4770d9ecef9f30a477
Dewayne VanHoozer <dewayne@mars.local>
2018-01-30 12:18:29 -0600

    reviewing options for HL7 message protocol processing

---
+++ HL7/.irbrc
@@ -0,0 +1,4 @@
+rq 'simple_hl7_parser'
+rq 'ruby-hl7'
+
+TEST_MESSAGE = Pathname.new('message.txt').read
---
+++ HL7/gems.rb
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+ruby 2.5.0
+
+gem 'ruby-hl7'                # Ruby HL7 Library
+gem 'simple_hl7_parser'       # Simple, extensible HL7 message parser
+
