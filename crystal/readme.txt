02556f798647a3e0d36c238567ea5aee85323c54
Dewayne VanHoozer <dvanhoozer@gmail.com>
2020-02-28 15:38:50 -0600

    something new with Time class

---
+++ crystal/readme.txt
@@ -0,0 +1,7 @@
+
+Note to self:
+
+crystal build pgm_name.cr --release #=> creates pgm_name executable
+
+crystal --help
+crystal command --help
--- crystal/time_me.cr
+++ crystal/time_me.cr
@@ -1,12 +1,22 @@
+# time_me.cr
 
-start_time = Time.now
+# Ruby ...
+# unless Time.respond_to? :measure
+#   class Time
+#     def self.measure
+#       start_time = Time.now
+#       yield
+#       return Time.now - start_time
+#     end
+#   end
+# end
 
 def my_random(upto)
   rand(upto)
 end
 
-100_000_000.times {|x| my_random(42)}
+elapsed_time = Time.measure do
+  100_000_000.times { |x| my_random(42) }
+end
 
-end_time = Time.now
-
-puts end_time - start_time
+puts elapsed_time
