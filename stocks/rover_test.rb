17872f7ee69baefdf213fb342c25cb525074ec6c
Dewayne VanHoozer <dvanhoozer@gmail.com>
2023-10-03 12:59:30 -0500

    snapshot

--- stocks/rover_test.rb
+++ stocks/rover_test.rb
@@ -1,10 +1,8 @@
 #!/usr/bin/env ruby
 
-require 'amazing_print'
 require 'debug_me'
 include DebugMe
 
-require 'csv'
 require 'pathname'
 
 require 'rover-df'
@@ -46,6 +44,7 @@ class NormalizeKeys
 end
 
 
+# NOTE: each key must be a Symbol
 def create_accessor_methods(df)
 	return if df.keys.empty?
 
@@ -64,25 +63,26 @@ aapl_csv = Pathname.pwd + 'aapl.csv'
 
 aapl_df = Rover.read_csv aapl_csv
 
-debug_me{[
-	"aapl_df.size",
-	"aapl_df.keys",
-	"aapl_df.methods"
-]}
-
-
 mapping = NormalizeKeys.call(aapl_df.keys)
-
-
-debug_me{[ :mapping ]}
 
 aapl_df.rename(mapping)
 
-debug_me{[ "aapl_df.keys" ]}
-
-debug_me{[ "aapl_df[:adj_close]" ]}
+debug_me{[
+  "aapl_df.keys",
+  "aapl_df[:adj_close]"
+]}
 
 create_accessor_methods(aapl_df)
 
 debug_me{[ "aapl_df.adj_close" ]}
 
+
+__END__
+
+# An example of a Historical Stock price CSV file download from Yahoo Finance:
+
+Date,Open,High,Low,Close,Adj Close,Volume
+2019-01-02,38.722500,39.712502,38.557499,39.480000,37.994484,148158800
+2019-01-03,35.994999,36.430000,35.500000,35.547501,34.209953,365248800
+2019-01-04,36.132500,37.137501,35.950001,37.064999,35.670361,234428400
+2019-01-07,37.174999,37.207500,36.474998,36.982498,35.590965,219111200
