df90f4b79466e43f2a72c7f9f798e66664269671
Dewayne VanHoozer <dvanhoozer@gmail.com>
2023-12-27 16:52:33 -0600

    I think this will work out

--- class_level_metadata/tools.rb
+++ class_level_metadata/tools.rb
@@ -1,36 +1,30 @@
 require 'hashie'
 
 class AIA::Tools
-  # Class variable to keep track of subclasses and their metadata
-  @@catalog = Hashie::Mash.new
+  # Class array to keep track of subclasses and their metadata
+  @@catalog = []
 
   class << self
-
     # Triggered when a subclass is created
     def inherited(subclass)
-      key = subclass.name.split('::').last.downcase
-
-      # Create metadata with the class name
-      subclass_meta = Hashie::Mash.new(klass: subclass )
-      
+      subclass_meta = Hashie::Mash.new(klass: subclass)
       subclass.instance_variable_set(:@_metadata, subclass_meta)
-      
-      # Add the subclass and its metadata to the catalog
-      # Use the class name as the key
-      @@catalog[key] = subclass_meta
+
+      # Add the subclass and its metadata to the catalog as a Hash
+      @@catalog << subclass_meta
     end
 
-
-    # Clas    # Instance method for setting or updating metadata
+    # Instance method for setting or updating metadata
     def meta(metadata = nil)
       # On retrieval, return the current metadata
       return @_metadata if metadata.nil?
-      
-      # On setting, update the _metadata and the entry in the catalog
+
+      # On setting, update the _metadata
       @_metadata = Hashie::Mash.new(metadata)
-      
-      key = self.name.split('::').last.downcase
-      @@catalog[key].merge! metadata
+
+      # Update the entry in the catalog
+      entry = @@catalog.detect { |item| item[:klass] == self }
+      entry.merge!(metadata) if entry
     end
 
     def get_meta
@@ -39,19 +33,15 @@ class AIA::Tools
 
     # Class method to search for subclasses matching certain criteria
     def search_for(criteria = {})
-      @@catalog.select do |name, meta|
-        criteria.all? do |k, v|
-          meta[k] == v
-        end
-      end # .map(&:values)
+      @@catalog.select do |meta|
+        criteria.all? { |k, v| meta[k] == v }
+      end
     end
-
 
     # Exposes the contents of the entire catalog
     def catalog
       @@catalog
     end
-
 
     # Method to load subclasses from a tools directory
     def load_subclasses
@@ -61,3 +51,5 @@ class AIA::Tools
     end
   end
 end
+
+
