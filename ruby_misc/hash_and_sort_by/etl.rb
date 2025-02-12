dce7e2c508c0738f65a5ad5133a7e80ae73533f9
Dewayne VanHoozer <dvanhoozer@gmail.com>
2025-02-12 01:26:43 -0600

    added comments

--- ruby_misc/hash_and_sort_by/etl.rb
+++ ruby_misc/hash_and_sort_by/etl.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
 # experiments/hash_and_sort_by/etl.rb
+# and ETL exercise
 #
 # Requirements:
+#   from the world of Harry Potter ....
 #   For each house, show the dude with the most friends.
-#   When a house has dues with the same maximum friend cout,
+#   When a house has dudes with the same maximum friend cout,
 #   select the dude whose name is first alphabetically.
 
 require "json"
@@ -12,6 +14,9 @@ require "json"
 data = JSON.parse(File.read('etl.json'))
 
 # Transform
+
+# Every time you allocate a new entry in this
+# Hash object, make its value an empty Array object
 temp = Hash.new { |h,k| h[k] = [] }
 
 data.each do |entry|
@@ -23,10 +28,21 @@ end
 
 # Transform Again and Load
 output  = temp.map do |house, dudes_with_friends|
-  king  = dudes_with_friends.sort_by {|h| [-h.values.first, h.keys.first ]}
-            .first.keys.first
+  dude  = dudes_with_friends
+            .sort_by { |h|  # sorting Hash objects **ascending**
+              [             # on two levels: number of friends, dude's name
+              # v=-< Don't miss the minus sign 
+              # v    (looking for the most friends)
+              # v    and the sort is ascending 
+                -h.values.first,  # an Array with one element
+                h.keys.first      # ditto
+              ]
+            }
+            .first  # Dude's entry with most friends count
+            .keys   # Array of Dude's names sorted alphabetically
+            .first  # this is the one that meets the requirement
             
-  { house => king}
+  { house => dude } # we're mapping, so this entry replaces the original
 end
 
 puts output
