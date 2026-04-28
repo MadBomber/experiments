2dc3541754ae6e4e2549c3f657a7b1e4066ddc48
Dewayne VanHoozer <dvanhoozer@gmail.com>
2026-04-28 15:49:05 -0500

    feat: add TypeCoercion for Ruby Array and Numo::NArray

--- ruby_misc/zfp/lib/zfp.rb
+++ ruby_misc/zfp/lib/zfp.rb
@@ -2,6 +2,8 @@
 
 require_relative "zfp/version"
 require_relative "zfp/errors"
+require_relative "zfp/ffi"
+require_relative "zfp/type_coercion"
 
 module Zfp
   # Add convenience methods here as subsequent tasks are completed
